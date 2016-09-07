5fc9268d18f41e9611217777176e95e05a7001cd
miss李 <jiushu0228@163.com>
2016-09-07 10:10:26 +0800

    delete

--- JSPatchDemo/AppDelegate.m
+++ JSPatchDemo/AppDelegate.m
@@ -62,7 +62,7 @@
 //    //调用系统能力 发送邮件
 //    NSURL *url = [NSURL URLWithString:[urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
 //    [[UIApplication sharedApplication] openURL:url];
-}- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
